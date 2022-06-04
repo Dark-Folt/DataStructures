c911cf88b3919dc2685345fa94869f6912cd032f
Dark-Folt <marigo9794@gmail.com>
2022-06-04 03:38:47 +0200

    rerfactor

--- linkedList/Node.c
+++ linkedList/Node.c
@@ -1,13 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "Node.h"
+
+bool
+node_empty (const Node_t *node)
+{
+    return (NULL == node);
+}
 
 Node_t *
 node_create (int value)
 {
     Node_t * node = (Node_t *) malloc(sizeof(Node_t));
-    if (NULL == node) {
+    if (node_empty (node)) {
         fprintf (stderr, "Erreur malloc de la node\n");
         exit (EXIT_FAILURE);
     }
@@ -17,13 +20,25 @@ node_create (int value)
     return node;
 }
 
+
 void
 node_free (Node_t *node)
 {
-    if (NULL == node) {
+    if (node_empty (node)) {
         fprintf(stderr, "La node n'est pas alloué\n");
         return;
     }
+    node->next = NULL;
     free (node);
     node = NULL;
+}
+
+void
+node_display(const Node_t *node)
+{
+    if (node_empty (node)) {
+        fprintf (stderr, "la node est vide. (NULL Pointer)\n");
+        return;
+    }
+    printf ("value: %d\t@: %p\tnext: %p\n", node->value, node, node->next);
 }
--- linkedList/Node.h
+++ linkedList/Node.h
@@ -1,6 +1,9 @@
 #ifndef _Node_H
 #define _Node_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 typedef struct _Node_t
 {
@@ -9,5 +12,9 @@ typedef struct _Node_t
 }Node_t;
 
 Node_t *node_create(int);
+void node_free(Node_t *);
+void node_display(const Node_t *);
+bool node_empty (const Node_t *);
+
 
 #endif
